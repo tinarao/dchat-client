13cd0327cb9ac6b5f820819283c67d60718b9b0c
tinarao <golovindanila1613@gmail.com>
2025-07-24 14:04:48 +0300

    made room switcher, declared types

--- lib/chat/types.ts
+++ lib/chat/types.ts
@@ -1,8 +1,24 @@
 export type User = {
+    id: number
+    bio?: string
     username: string
+    avatarUrl?: string
+    rooms: Room[]
+
+    // timestamps included too
+}
+
+export type Room = {
+    id: number
+    messages: Message[]
+    // timestamps included too
 }
 
 export type Message = {
+    id: number
     user: User
+    room: Room
     content: string
+
+    // timestamps included too
 } 
--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,84 @@
 <script setup lang="ts">
-import { Channel, Socket } from "phoenix"
-import type { Message } from "~/lib/chat/types"
+import { type Channel, Socket } from "phoenix";
+import type { Message } from "~/lib/chat/types";
 
-const channel = ref<Channel | null>(null)
-const message = ref("")
-const messages = ref<Message[]>([])
-const currentUser = ref('Anonymous')
+const channel = ref<Channel | null>(null);
+const message = ref("");
+const messages = ref<Message[]>([]);
+const currentUser = ref("Anonymous");
+const room = ref("room:lobby");
 
-const socket = ref<Socket | null>(null)
+const socket = ref<Socket | null>(null);
+const route = useRoute();
 
 function initSocket() {
-    if (!socket.value) return
-    channel.value = socket.value.channel("room:lobby", {});
+    if (!socket.value) return;
 
-    channel.value.join()
-        .receive('ok', ({ messages: initialMessages }) => {
+    channel.value = socket.value.channel(room.value, {});
+
+    channel.value
+        .join()
+        .receive("ok", ({ messages: _initialMessages }) => {
             // messages.value = initialMessages
         })
-        .receive('error', ({ error }) => {
-            console.error(error)
-        })
+        .receive("error", ({ error }) => {
+            console.error(error);
+        });
 
-    channel.value.on('new_message', (msg: Message) => {
-        console.log(msg)
-        messages.value.push(msg)
-    })
+    channel.value.on("new_message", (msg: Message) => {
+        console.log(msg);
+        messages.value.push(msg);
+    });
 }
 
 const sendMessage = () => {
-    if (!channel.value) return
-    if (message.value.trim() === '') return
+    if (!channel.value) return;
+    if (message.value.trim() === "") return;
 
-    channel.value.push('new_message', {
+    channel.value.push("new_message", {
         user: currentUser.value,
-        content: message.value
-    })
+        content: message.value,
+    });
 
-    message.value = ''
-}
+    message.value = "";
+};
 
-onMounted(() => {
-    messages.value = []
-    socket.value = new Socket("ws://localhost:4000/chat", {
-        params: {
-            token: "penis"
-        }
-    })
-
-    if (socket.value) {
-        socket.value.connect()
+onMounted(async () => {
+    const roomParam = route.query.room;
+    if (!roomParam) {
+        await navigateTo("/?room=lobby");
+        room.value = "room:lobby";
+    } else {
+        room.value = "room:" + roomParam;
     }
 
-    console.log("Connected")
-    initSocket()
-})
+    messages.value = [];
+    socket.value = new Socket("ws://localhost:4000/chat", {
+        params: {
+            token: "penis",
+        },
+    });
+
+    if (socket.value) {
+        socket.value.connect();
+    }
+
+    console.log("Connected");
+    initSocket();
+});
 
 onUnmounted(() => {
-    socket.value?.disconnect()
-})
+    socket.value?.disconnect();
+});
 </script>
 
 <template>
     <div>
-        <p>
-            Текущая комната: {{ channel?.topic }}
-        </p>
+        <p>Текущая комната: {{ channel?.topic }}</p>
     </div>
     <div>
         <textarea v-model="message" />
-        <button @click="sendMessage">
-            Отправить
-        </button>
+        <button @click="sendMessage">Отправить</button>
     </div>
     <div>
         <ul>
